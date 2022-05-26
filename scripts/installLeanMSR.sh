0e22e81271b947a89cf32ad7956d4c750d021cc3
WmDevOps <wmdevops@training.nowhere>
2022-05-26 14:10:22 +0000

    Install MSR

---
+++ scripts/installLeanMSR.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. ${BUILD_SOURCESDIRECTORY}/scripts/setEnv.sh
+. ${SUIF_HOME}/01.scripts/commonFunctions.sh
+. ${SUIF_HOME}/01.scripts/installation/setupFunctions.sh
+
+logI "Greating "
+sudo chmod a+w ${SUIF_INSTALL_INSTALL_DIR}
+
+logI "SUIF env before installation of MSR:"
+env | grep SUIF_ | sort
+
+logI "Installing MSR..."
+
+applySetupTemplate "${MY_MSR_template}"
+
+local installResult=$?
+
+if [ "${installResult}" -ne 0 ]; then
+  logE "Installation failed, code ${installResult}"
+  exit 1
+fi
+
+logI "MSR installation successful"
